73f2d3ace38485b4bb053ba498055ebef2f483a6
DimaWoic <woicdv@gmail.com>
2021-03-03 19:18:49 +0300

    в  models.py добавлены модели EbGroup - группа по элетробезопасности, Ticket- № билета, Question - вопросы билета

--- models.py
+++ models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class EbGroup(models.Model):
+    name = models.CharField(max_length=300, verbose_name='наименование')
+
+    class Meta:
+        verbose_name = 'группа по электробезопасности'
+        verbose_name_plural = 'группы по электробезопасности'
+
+    def __str__(self):
+        return self.name
+
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=10, verbose_name='номер билета', unique=True)
+    eb_group = models.ForeignKey(on_delete=models.CASCADE, verbose_name='группа по электробезопасности')
+
+    class Meta:
+        verbose_name = 'номер билета'
+        verbose_name_plural = 'номера билетов'
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    text = models.TextField(verbose_name='текст вопроса', max_length=600)
+    ticket = models.ForeignKey(verbose_name='номер билета', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
+
+    def __str__(self):
+        return self.text
